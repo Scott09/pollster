81b8d1a4b2e3e140768d67796349ccddbd229535
Scott Appleton <Scottappleton09@gmail.com>
2019-07-28 21:11:18 +0000

    add post for /polls route

--- routes/polls.js
+++ routes/polls.js
@@ -5,6 +5,10 @@ module.exports = (db) => {
   router.get("/", (request, response) => {
     response.render("index");
   });
-  
+  router.post("/", (request, response) => {
+    console.log("Posting on polls");
+    response.redirect("results");
+  });
+
   return router;
 };
