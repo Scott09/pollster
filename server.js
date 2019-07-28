8f822eeb84cb4cb82a4f83a02d8184e8f4b7d0c0
Scott Appleton <Scottappleton09@gmail.com>
2019-07-28 18:31:59 +0000

    added /polls route

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ const widgetsRoutes = require("./routes/widgets");
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
 
