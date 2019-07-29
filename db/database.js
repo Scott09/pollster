4fb08b349faa536712d9de30ce388523099b9700
Jiadan <jiadanwang@gmail.com>
2019-07-29 04:19:34 +0000

    add couple functions for database

---
+++ db/database.js
@@ -0,0 +1,70 @@
+const {pool} = require ('pg');
+
+const pool = new pool ({
+  user: 'labber',
+  host: 'localhost',
+  password: 'password',
+  database: 'midterm'
+})
+
+pool.connect();
+
+// getVotesBy
+
+
+//Links???
+const addPoll = function(poll) {
+  return new Promise((resolve,reject) => {
+    pool.query(
+      `INSERT INTO polls (owner_name, owner_email, question)
+      VALUES (${poll.owner_name},${poll.owner_email},${polls.question})
+      RETURNING *;`)
+    .then((result) => {
+      resolve(result.row[0])
+    })
+  })
+}
+exports.addPoll = addPoll;
+
+
+const addVoter = function(voter) {
+  return new Promise((resolve, reject) =>{
+    pool.query(
+      `INSERT INTO voters (
+        name, polls_id )
+        VALUES (${voter.name}, ${voter.polls_id})
+      RETURNING *;`)
+      .then((result) => {
+        resolve(result.row[0])
+      })
+  })
+}
+exports.addVoter = addVoter;
+
+const addVote = function(vote) {
+  return new Promise((resolve, reject) =>{
+    pool.query(
+      `INSERT INTO votes (voter_id,choice_id, rank)
+      VALUES (${vote.voter_id}, ${vote.choice_id},${vote.rank})
+      RETURNING *;`)
+      .then((result) => {
+        resolve(result.row[0])
+      })
+  })
+}
+
+exports.addVote = addVote;
+
+const addChoice = function(choice) {
+  return new Promise((resolve, reject) =>{
+    pool.query(
+      `INSERT INTO  choices ( poll_id,description, title)
+      VALUES (${choice.poll_id}, ${choice.description},${choice.title})
+      RETURNING *;`)
+      .then((result) => {
+        resolve(result.row[0])
+      })
+  })
+}
+
+exports.addChoice = addChoice;
