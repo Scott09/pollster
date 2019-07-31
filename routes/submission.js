9d6685afa69658156fe6d357d0c8e0517cbb7ff4
Aidan <your@email.com>
2019-07-31 18:28:45 +0000

    Inserted choice_rankings into DB and added name field

--- routes/submission.js
+++ routes/submission.js
@@ -1,14 +1,29 @@
 const express = require('express');
-const router = express.Router();
-const { getPollAndChoicesByID } = require('../db/database');
+const router = express.Router({mergeParams: true});
+const { getPollAndChoicesByID, submitPollVote } = require('../db/database');
+const { pool } = require('../lib/db');
+
 
 router.get("/new", (req, resp) => {
   getPollAndChoicesByID(req.params.poll_id)
     .then(poll => {
-      console.log(poll);
       resp.render('../views/submission.ejs', {poll});
     })
-    .catch(err => { throw new Error(err)});
+    .catch(err => {
+      throw new Error(err);
+    });
 });
 
+router.post("/", (req, resp) => {
+  submitPollVote(req.params.poll_id, req.body.name, req.body.choices.split(',')).then(() => {
+    resp.send("ok");
+  }).catch(err => {
+    throw new Error(err);
+  });
+});
+
+
+
 module.exports = router;
+
+//solve :poll_id route dynamicism
